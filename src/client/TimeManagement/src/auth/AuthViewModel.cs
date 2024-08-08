e68de96e7a11912677658ae1086da0f0eed7c855
MrGodOfNight <DenRussianBoy@mail.ru>
2024-08-08 16:41:15 +0500

    add license to .cs files

--- src/client/TimeManagement/src/auth/AuthViewModel.cs
+++ src/client/TimeManagement/src/auth/AuthViewModel.cs
@@ -1,4 +1,29 @@
-﻿using MsBox.Avalonia.Enums;
+﻿/*
+	MIT License
+
+	Copyright (c) 2024 Ushakov Igor
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+*/
+
+using MsBox.Avalonia.Enums;
 using MsBox.Avalonia;
 using System;
 using System.Collections.Generic;
@@ -56,7 +81,6 @@ namespace TimeManagement.src.auth
             string jsonContent = JsonManager.LoadJsonFile("TimeManagement.src.localization.localization.json");
             _localization = new Localizer(jsonContent);
             CultureInfo currentCulture = CultureInfo.CurrentCulture;
-            //TODO: make the default language on the view
             switch (currentCulture.Name)
             {
                 case "en-US":
