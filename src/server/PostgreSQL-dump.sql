63488523b30b1c130b5d2a1e0095df03386915da
MrGodOfNight <DenRussianBoy@mail.ru>
2024-08-24 22:55:49 +0500

    update db schema

--- src/server/PostgreSQL-dump.sql
+++ src/server/PostgreSQL-dump.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database cluster dump
 --
 
--- Started on 2024-07-26 00:29:27
+-- Started on 2024-08-24 22:55:24
 
 SET default_transaction_read_only = off;
 
@@ -44,7 +44,7 @@ ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION
 -- Dumped from database version 16.0
 -- Dumped by pg_dump version 16.0
 
--- Started on 2024-07-26 00:29:27
+-- Started on 2024-08-24 22:55:24
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -57,7 +57,7 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
--- Completed on 2024-07-26 00:29:28
+-- Completed on 2024-08-24 22:55:24
 
 --
 -- PostgreSQL database dump complete
@@ -74,7 +74,7 @@ SET row_security = off;
 -- Dumped from database version 16.0
 -- Dumped by pg_dump version 16.0
 
--- Started on 2024-07-26 00:29:28
+-- Started on 2024-08-24 22:55:24
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -88,7 +88,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 4810 (class 1262 OID 25565)
+-- TOC entry 4817 (class 1262 OID 25565)
 -- Name: time; Type: DATABASE; Schema: -; Owner: postgres
 --
 
@@ -115,6 +115,56 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- TOC entry 222 (class 1259 OID 33716)
+-- Name: break_time; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.break_time (
+    id integer NOT NULL,
+    date date DEFAULT CURRENT_DATE NOT NULL,
+    start_time time without time zone,
+    end_time time without time zone,
+    user_id integer NOT NULL
+);
+
+
+ALTER TABLE public.break_time OWNER TO postgres;
+
+--
+-- TOC entry 4818 (class 0 OID 0)
+-- Dependencies: 222
+-- Name: TABLE break_time; Type: COMMENT; Schema: public; Owner: postgres
+--
+
+COMMENT ON TABLE public.break_time IS 'this table for break time';
+
+
+--
+-- TOC entry 221 (class 1259 OID 33715)
+-- Name: break_time_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.break_time_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.break_time_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 4819 (class 0 OID 0)
+-- Dependencies: 221
+-- Name: break_time_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.break_time_id_seq OWNED BY public.break_time.id;
+
+
+--
 -- TOC entry 220 (class 1259 OID 25589)
 -- Name: report; Type: TABLE; Schema: public; Owner: postgres
 --
@@ -122,14 +172,15 @@ SET default_table_access_method = heap;
 CREATE TABLE public.report (
     id integer NOT NULL,
     text text NOT NULL,
-    time_id integer NOT NULL
+    time_id integer NOT NULL,
+    creation_time time without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 
 ALTER TABLE public.report OWNER TO postgres;
 
 --
--- TOC entry 4811 (class 0 OID 0)
+-- TOC entry 4820 (class 0 OID 0)
 -- Dependencies: 220
 -- Name: TABLE report; Type: COMMENT; Schema: public; Owner: postgres
 --
@@ -138,7 +189,7 @@ COMMENT ON TABLE public.report IS 'table that stores all reports in markdown for
 
 
 --
--- TOC entry 4812 (class 0 OID 0)
+-- TOC entry 4821 (class 0 OID 0)
 -- Dependencies: 220
 -- Name: COLUMN report.text; Type: COMMENT; Schema: public; Owner: postgres
 --
@@ -163,7 +214,7 @@ CREATE SEQUENCE public.report_id_seq
 ALTER SEQUENCE public.report_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4813 (class 0 OID 0)
+-- TOC entry 4822 (class 0 OID 0)
 -- Dependencies: 219
 -- Name: report_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -182,7 +233,7 @@ CREATE TABLE public."user" (
     password text NOT NULL,
     reg_date date DEFAULT CURRENT_DATE NOT NULL,
     end_date date NOT NULL,
-    last_time timestamp without time zone NOT NULL,
+    last_time timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     admin integer
 );
 
@@ -190,7 +241,7 @@ CREATE TABLE public."user" (
 ALTER TABLE public."user" OWNER TO postgres;
 
 --
--- TOC entry 4814 (class 0 OID 0)
+-- TOC entry 4823 (class 0 OID 0)
 -- Dependencies: 216
 -- Name: TABLE "user"; Type: COMMENT; Schema: public; Owner: postgres
 --
@@ -199,7 +250,7 @@ COMMENT ON TABLE public."user" IS 'this table of user accounts';
 
 
 --
--- TOC entry 4815 (class 0 OID 0)
+-- TOC entry 4824 (class 0 OID 0)
 -- Dependencies: 216
 -- Name: COLUMN "user".reg_date; Type: COMMENT; Schema: public; Owner: postgres
 --
@@ -208,7 +259,7 @@ COMMENT ON COLUMN public."user".reg_date IS 'registration date';
 
 
 --
--- TOC entry 4816 (class 0 OID 0)
+-- TOC entry 4825 (class 0 OID 0)
 -- Dependencies: 216
 -- Name: COLUMN "user".end_date; Type: COMMENT; Schema: public; Owner: postgres
 --
@@ -217,7 +268,7 @@ COMMENT ON COLUMN public."user".end_date IS 'account expiration date';
 
 
 --
--- TOC entry 4817 (class 0 OID 0)
+-- TOC entry 4826 (class 0 OID 0)
 -- Dependencies: 216
 -- Name: COLUMN "user".last_time; Type: COMMENT; Schema: public; Owner: postgres
 --
@@ -226,7 +277,7 @@ COMMENT ON COLUMN public."user".last_time IS 'last login time';
 
 
 --
--- TOC entry 4818 (class 0 OID 0)
+-- TOC entry 4827 (class 0 OID 0)
 -- Dependencies: 216
 -- Name: COLUMN "user".admin; Type: COMMENT; Schema: public; Owner: postgres
 --
@@ -251,7 +302,7 @@ CREATE SEQUENCE public.users_id_seq
 ALTER SEQUENCE public.users_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4819 (class 0 OID 0)
+-- TOC entry 4828 (class 0 OID 0)
 -- Dependencies: 215
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -266,11 +317,9 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public."user".id;
 
 CREATE TABLE public.work_time (
     id integer NOT NULL,
-    date date NOT NULL,
+    date date DEFAULT CURRENT_DATE NOT NULL,
     start_time time without time zone,
     end_time time without time zone,
-    start_lunch time without time zone,
-    end_lunch time without time zone,
     user_id integer NOT NULL
 );
 
@@ -294,7 +343,7 @@ CREATE SEQUENCE public.work_time_id_seq
 ALTER SEQUENCE public.work_time_id_seq OWNER TO postgres;
 
 --
--- TOC entry 4820 (class 0 OID 0)
+-- TOC entry 4829 (class 0 OID 0)
 -- Dependencies: 217
 -- Name: work_time_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -303,7 +352,15 @@ ALTER SEQUENCE public.work_time_id_seq OWNED BY public.work_time.id;
 
 
 --
--- TOC entry 4647 (class 2604 OID 25592)
+-- TOC entry 4656 (class 2604 OID 33719)
+-- Name: break_time id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.break_time ALTER COLUMN id SET DEFAULT nextval('public.break_time_id_seq'::regclass);
+
+
+--
+-- TOC entry 4654 (class 2604 OID 25592)
 -- Name: report id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -311,7 +368,7 @@ ALTER TABLE ONLY public.report ALTER COLUMN id SET DEFAULT nextval('public.repor
 
 
 --
--- TOC entry 4644 (class 2604 OID 25570)
+-- TOC entry 4649 (class 2604 OID 25570)
 -- Name: user id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -319,7 +376,7 @@ ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.users
 
 
 --
--- TOC entry 4646 (class 2604 OID 25580)
+-- TOC entry 4652 (class 2604 OID 25580)
 -- Name: work_time id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -327,64 +384,16 @@ ALTER TABLE ONLY public.work_time ALTER COLUMN id SET DEFAULT nextval('public.wo
 
 
 --
--- TOC entry 4804 (class 0 OID 25589)
--- Dependencies: 220
--- Data for Name: report; Type: TABLE DATA; Schema: public; Owner: postgres
+-- TOC entry 4665 (class 2606 OID 33722)
+-- Name: break_time break_time_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-COPY public.report (id, text, time_id) FROM stdin;
-\.
-
-
---
--- TOC entry 4800 (class 0 OID 25567)
--- Dependencies: 216
--- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public."user" (id, login, password, reg_date, end_date, last_time, admin) FROM stdin;
-\.
+ALTER TABLE ONLY public.break_time
+    ADD CONSTRAINT break_time_pk PRIMARY KEY (id);
 
 
 --
--- TOC entry 4802 (class 0 OID 25577)
--- Dependencies: 218
--- Data for Name: work_time; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.work_time (id, date, start_time, end_time, start_lunch, end_lunch, user_id) FROM stdin;
-\.
-
-
---
--- TOC entry 4821 (class 0 OID 0)
--- Dependencies: 219
--- Name: report_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.report_id_seq', 1, false);
-
-
---
--- TOC entry 4822 (class 0 OID 0)
--- Dependencies: 215
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.users_id_seq', 1, false);
-
-
---
--- TOC entry 4823 (class 0 OID 0)
--- Dependencies: 217
--- Name: work_time_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.work_time_id_seq', 1, false);
-
-
---
--- TOC entry 4653 (class 2606 OID 25596)
+-- TOC entry 4663 (class 2606 OID 25596)
 -- Name: report report_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -393,7 +402,7 @@ ALTER TABLE ONLY public.report
 
 
 --
--- TOC entry 4649 (class 2606 OID 25575)
+-- TOC entry 4659 (class 2606 OID 25575)
 -- Name: user users_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -402,7 +411,7 @@ ALTER TABLE ONLY public."user"
 
 
 --
--- TOC entry 4651 (class 2606 OID 25582)
+-- TOC entry 4661 (class 2606 OID 25582)
 -- Name: work_time work_time_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -411,7 +420,16 @@ ALTER TABLE ONLY public.work_time
 
 
 --
--- TOC entry 4655 (class 2606 OID 25597)
+-- TOC entry 4668 (class 2606 OID 33723)
+-- Name: break_time break_time_user_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.break_time
+    ADD CONSTRAINT break_time_user_fk FOREIGN KEY (user_id) REFERENCES public."user"(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- TOC entry 4667 (class 2606 OID 25597)
 -- Name: report report_work_time_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -420,7 +438,7 @@ ALTER TABLE ONLY public.report
 
 
 --
--- TOC entry 4654 (class 2606 OID 25583)
+-- TOC entry 4666 (class 2606 OID 25583)
 -- Name: work_time work_time_users_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -428,13 +446,13 @@ ALTER TABLE ONLY public.work_time
     ADD CONSTRAINT work_time_users_fk FOREIGN KEY (user_id) REFERENCES public."user"(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
--- Completed on 2024-07-26 00:29:28
+-- Completed on 2024-08-24 22:55:24
 
 --
 -- PostgreSQL database dump complete
 --
 
--- Completed on 2024-07-26 00:29:28
+-- Completed on 2024-08-24 22:55:24
 
 --
 -- PostgreSQL database cluster dump complete
